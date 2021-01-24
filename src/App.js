a9215d8d69ef63997f8646e855bb5a8300b69609
AshishKumarSinghSengar <65999078+AshishKumarSinghSengar@users.noreply.github.com>
2021-01-24 10:53:37 +0530

    Intital Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './Components/MainComponent';
+
+function App() {
+  return (
+    <div className="App">
+        <Main/>
+    </div>
+  );
+}
+
+export default App;

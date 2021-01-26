1b14e580ec1e785c897d563773ed50906e9e9b5a
AshishKumarSinghSengar <65999078+AshishKumarSinghSengar@users.noreply.github.com>
2021-01-26 12:34:31 +0530

    Landing Page inco

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Main from "./Components/MainComponent";
+import Loader from './Components/loaderComponent';
 import './App.css';
-import Main from './Components/MainComponent';
 
 function App() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setIsLoading(false);
+    },4000);
+  })
+
   return (
-    <div className="App">
+    <div>
+      {
+        isLoading==true?
+        <Loader/>:
         <Main/>
+      }
     </div>
   );
 }
 
 export default App;
+/*
+
+*/
--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import First from './firstPage';
 
 class Main extends Component{
     render(){
         return(
             <div>
-
+                <First/>
             </div>
         );
     }

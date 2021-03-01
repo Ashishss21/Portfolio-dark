451a19de58d8349c1ec8100fa44cc22be37b3bc4
AshishKumarSinghSengar <65999078+AshishKumarSinghSengar@users.noreply.github.com>
2021-03-01 22:15:39 +0530

    Mobile Nav toggle working

--- src/Components/sideBar.js
+++ src/Components/sideBar.js
@@ -2,8 +2,27 @@ import React, { Component } from 'react';
 import '../style/navbar.css';
 import {Link} from 'react-router-dom';
 import PDF from '../Assests/RESUME.pdf';
+import $ from 'jquery';
 
 class Sidebar extends Component {
+
+    componentDidMount() {
+        $(document).on('click', '.mobile-nav-toggle', function (e) {
+            $('body').toggleClass('mobile-nav-active');
+            $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        });
+
+        $(document).click(function(e) {
+            var container = $(".mobile-nav-toggle");
+            if (!container.is(e.target) && container.has(e.target).length === 0) {
+              if ($('body').hasClass('mobile-nav-active')) {
+                $('body').removeClass('mobile-nav-active');
+                $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+              }
+            }
+          });
+    }
+
     render() {
         return (
             <div>

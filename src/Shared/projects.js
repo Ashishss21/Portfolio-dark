dbf6c9872be9dbdcabea42c5ed2099f18be4cc5b
Ashish Sengar <65999078+Ashishss21@users.noreply.github.com>
2021-07-16 21:41:37 +0530

    adding new project

--- src/Shared/projects.js
+++ src/Shared/projects.js
@@ -10,7 +10,7 @@ export const PROJECTS = [
     {
         id: 2,
         name: 'Covid-19',
-        image: '/images/back2.png',
+        image: '/images/back3.png',
         heading: 'Covid-19 Tracker App',
         link: 'https://www.github.com/AshishKumarSinghSengar/CovidTracker-boot', 
         description: 'Covid tracker is a website built using Spring Boot'
@@ -18,10 +18,10 @@ export const PROJECTS = [
     {
         id: 3,
         name: 'Online Examination System',
-        image: '/images/back3.png',
+        image: '/images/back2.png',
         heading: 'Examination System',
         link: 'https://www.github.com/AshishKumarSinghSengar/Online-Quiz-Application', 
-        description: 'Online Examination System is a website built using HTML-PHP-JAVASCRIPT. It is a Group Project developed during 2020 internship.'
+        description: 'Online Examination System is a website built using HTML-PHP-JAVASCRIPT.'
     },
     {
         id: 4,
@@ -29,6 +29,13 @@ export const PROJECTS = [
         image: '/images/back4.png',
         heading: 'Online Chat-App',
         link: 'https://chatapp-sample.netlify.app/', 
-        description: 'Online chat application build using ReactJs'
+        description: 'Online chat application'
+    },{
+        id: 5,
+        name: 'Image-Gallery',
+        image: '/images/back5.png',
+        heading: 'Image-Gallery',
+        link: 'https://priceless-liskov-d98902.netlify.app/', 
+        description: 'Image gallery build using ReactJs and TailwindCSS'
     }
 ]
